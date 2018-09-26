ea2598de997f9749e366903eafb77dbfddeb3e4c
titov-andrei <titov_andrei@mail.ru>
2018-09-26 22:45:29 +0300

    Убираем код возврата

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 if (process.argv[2] === `--version`) {
   console.log(`v0.0.1`);
-  process.exit(0);
 } else if (process.argv[2] === `--help`) {
     console.log(`
       Доступные команды:
       --help— печатает этот текст;
       --version— печатает версию приложения;
       `);
-    process.exit(0);
 } else if (!process.argv[2]) {
     console.log(`
       Привет пользователь!
       Эта программа будет запускать сервер «Кексобукинг».
       Автор: Кекс.
       `);
-    process.exit(0);
 } else if ((process.argv[2] !== `--help`) || (process.argv[2] !== `--version`)) {
     console.log(`
       Неизвестная команда {{ ${process.argv[2]} }}.
